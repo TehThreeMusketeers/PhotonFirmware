2398d3029c2ca936c1ab8f93da4d43c274525849
c_chistol@yahoo.co.uk <c_chistol@yahoo.co.uk>
2018-03-10 16:20:59 +0000

    added code to the actuators to handle smart plug

--- Actuators/src/Lights_Kettle_Plug.ino
+++ Actuators/src/Lights_Kettle_Plug.ino
@@ -1,12 +1,27 @@
+
+#include <HttpClient.h>
+#include <Particle.h>
 PRODUCT_ID(6812);
-PRODUCT_VERSION(9);
+PRODUCT_VERSION(10);
+
+
+// HTTP communication
+HttpClient http;
+http_request_t request;
+http_response_t response;
+http_header_t headers[] = {
+  {"Accept", "text/plain"},
+  {NULL,NULL}
+};
 
 // command stubs
 String lightCommand = "APP#AC:CF:23:28:C2:2C#CMD#";
 String kettleCommand = "set sys output ";
+String plugCommand = "/cgi-bin/relay.cgi?";
 // exposed cloud functions
 int setLight(String cmd);
 int setKettle(String cmd);
+int setPlug(String cmd);
 
 // comms
 TCPClient rssiClient;
@@ -14,6 +29,8 @@ byte lightAddress[] = {178,62,58,245};
 int lightPORT = 38899;
 byte kettleAddress[] = {192,168,0,104};
 int kettlePort = 2000;
+
+String tst;
 
 void setup()
 {
@@ -24,6 +41,7 @@ void setup()
     // register the cloud function
     Particle.function("setLight", setLight);
     Particle.function("setKettle", setKettle);
+    Particle.function("setPlug", setPlug);
 }
 
 void loop() {}
@@ -33,16 +51,36 @@ int setLight(String cmd)
   String temp = lightCommand;
   temp += cmd + "\n";
   sendMessage(temp, lightAddress, lightPORT);
-  Serial.println(temp);
 }
 
 int setKettle(String cmd)
 {
   String temp = kettleCommand;
   temp += cmd + "\n";
-  Serial.println(temp);
   sendMessage(temp, kettleAddress, kettlePort);
-  return 1;
+}
+
+int setPlug(String cmd)
+{
+  String temp = plugCommand + cmd;
+  callPlug(temp);
+}
+
+void callPlug(String cmd)
+{
+  request.hostname = "192.168.0.107";
+  request.port = 80;
+  request.path = cmd;
+  http.get(request, response, headers);
+  //printResponse(response);
+}
+
+// print the data returned from the server for development and testing purposes
+void printResponse(http_response_t &response)
+{
+  Serial.println("HTTP Response: ");
+  Serial.println(response.status);
+  Serial.println(response.body);
 }
 
 void sendMessage(String signal, byte server[], int port)
@@ -53,11 +91,11 @@ void sendMessage(String signal, byte server[], int port)
     {
       rssiClient.println(signal);
     }
-    rssiClient.flush();
-    rssiClient.stop();
   }
   else
   {
     Serial.println("Cannot connect to server");
   }
+  rssiClient.flush();
+  rssiClient.stop();
 }
